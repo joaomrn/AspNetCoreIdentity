7f147252b21435c91438dbd67e5dae809686b08e
João Vitor <joaovitormrn@gmail.com>
2021-04-17 17:21:29 -0300

    Add project files.

---
+++ AspNetCoreIdentity/Config/DependemcyInjectionConfig.cs
@@ -0,0 +1,18 @@
+﻿using AspNetCoreIdentity.Extensions;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AspNetCoreIdentity.Config
+{
+    public static class DependemcyInjectionConfig
+    {
+        public static IServiceCollection ResolvedorDependencies(this IServiceCollection services)
+        {
+            services.AddSingleton<IAuthorizationHandler, PermissaoNecessariaHandler>();
+
+            services.AddScoped<AuditoriaFilter>();
+
+            return services;
+        }
+    }
+}
---
+++ AspNetCoreIdentity/Extensions/CustomAuthorization.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Routing;
+using System.Linq;
+using System.Security.Claims;
+
+namespace AspNetCoreIdentity.Extensions
+{
+    public class CustomAuthorization
+    {
+        public static bool ValidarClaimsUsuario(HttpContext context, string claimName, string claimValues)
+        {
+            return context.User.Identity.IsAuthenticated &&
+                   context.User.Claims.Any(c => c.Type == claimName && c.Value.Contains(claimValues));
+        }
+    }
+
+    public class ClaimAuthorizeAttribute : TypeFilterAttribute
+    {
+        public ClaimAuthorizeAttribute(string claimName, string claimValues) : base(typeof(RequisitoClaimFilter))
+        {
+            Arguments = new object[] { new Claim(claimName, claimValues) };
+        }
+    }
+
+    public class RequisitoClaimFilter : IAuthorizationFilter
+    {
+        private readonly Claim _claim;
+
+        public RequisitoClaimFilter(Claim claim)
+        {
+            _claim = claim;
+        }
+
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            if (!context.HttpContext.User.Identity.IsAuthenticated)
+            {
+                context.Result = new RedirectToRouteResult(new RouteValueDictionary(new
+                {
+                    area = "Identity",
+                    page = "/Account/Login",
+                    ReturnUrl = context.HttpContext
+                }));
+
+                return;
+            }
+
+            if (!CustomAuthorization.ValidarClaimsUsuario(context.HttpContext, _claim.Type, _claim.Value))
+            {
+                context.Result = new StatusCodeResult(403);
+            }
+        }
+    }
+}
